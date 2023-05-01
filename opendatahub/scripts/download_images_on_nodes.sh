9ae626047db9a911184478800e5230c769428837
Jooho Lee <jlee@redhat.com>
2023-05-01 17:29:50 -0400

    Scripts fix (#93)
    * fix scripts for e2e
    
    * add odh-model-controller modelmesh-controller to download images

--- opendatahub/scripts/download_images_on_nodes.sh
+++ opendatahub/scripts/download_images_on_nodes.sh
@@ -52,6 +52,12 @@ if [[ z${img_name} != z ]]; then
       info ".. rest-proxy image is set"
       export REST_PROXY=${img_url}
       ;;
+    modelmesh-controller)
+      info ".. modelmesh-controller image is set"
+      ;;
+    odh-model-controller)
+      info ".. odh-model-controller image is set"
+      ;;
     *)
       echo "No components found"
       exit 1
